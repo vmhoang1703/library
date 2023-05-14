8ae5859ed17fd9487532b9a2f8b55ececf28b216
Vu Minh Hoang <vmhoang1703@gmail.com>
2023-05-14 21:20:17 +0700

    structure

--- routes/gameRouter.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const gameController = require('../controllers/gameController');
-
-// router.get('/slug', gameController.show);
-
-module.exports = router;
--- routes/siteRouter.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const siteController = require('../controllers/siteController');
-
-router.get('/', siteController.index);
-
-module.exports = router;
